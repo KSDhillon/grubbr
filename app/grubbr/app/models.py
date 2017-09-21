367a3dda31c4a3e5517e9c96999a8f1eb639a448
Karan <Karanvirsdhillon@gmail.com>
2017-09-21 10:42:29 -0400

    models updated

--- app/grubbr/app/models.py
+++ app/grubbr/app/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 
 class User(models.Model):
-    email = model.EmailField(max_length=150, unique=True)
-    password = model.CharField(max_length=200)
-    street = model.CharField(max_length=300)
-    city = model.CharField(max_length=100)
-    state = model.CharField(max_length=50)
-    zipcode = model.CharField(max_length=5)
+    email = models.EmailField(max_length=150, unique=True)
+    password = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    street = models.CharField(max_length=300)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=5)
 
 class Meal(models.Model):
     cook = models.ForeignKey(User, on_delete=models.CASCADE)
+    buyer = models.ManytoManyField(User)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     portions = models.IntegerField(default=1)
-    
+    street = models.CharField(max_length=300)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=5)
+    pickup_time_start = models.DateTimeField()
+    pickup_time_end = models.DateTimeField()
