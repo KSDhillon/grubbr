571b04df30d080fcff59896296f6ddcdd870849a
dankramp <danielbkramp@gmail.com>
2017-10-05 17:22:21 -0400

    Editing testS

--- grubbr_models/models/tests.py
+++ grubbr_models/models/tests.py
@@ -1,59 +1,59 @@
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
-from models import Order, User
+from .models import User
 import urllib.request
 import urllib.parse
 import json
 
-    class UserTestCase(TestCase):
-        #setUp method is called before each test in this class
-        def setUp(self):
-            self.first_user = User.objects.create(email = "test@example.com", password = "password", first_name = "first", last_name = "user")
-            
-            sample_data = {'email': 'testp@virginia.edu',
-                         'password': 'password',
-                         'first_name': 'tester',
-                         'last_name': 'testing'
-            }
+class UserTestCase(TestCase):
+    #setUp method is called before each test in this class
+    def setUp(self):
+        self.first_user = User.objects.create(email = "test@example.com", password = "password", first_name = "first", last_name = "user")
         
-        def create_user(self):
-            url = reverse('user_list')
-            user_data = {'email': 'dankramp@virginia.edu',
-                         'password': 'password',
-                         'first_name': 'Dan',
-                         'last_name': 'Kramp'
-            }
+        sample_data = {'email': 'testp@virginia.edu',
+                       'password': 'password',
+                       'first_name': 'tester',
+                       'last_name': 'testing'
+        }
+        
+    def test_create_user(self):
+        url = reverse('user_create')
+        user_data = {'email': 'dankramp@virginia.edu',
+                     'password': 'password',
+                     'first_name': 'Dan',
+                     'last_name': 'Kramp'
+        }
+        
+        response = self.client.post(url, user_data, format='json')
+        
+        self.assertEqual(User.objects.count(), 2)
+        new_user = User.objects.get(pk=2)
+        self.assertEqual(new_user.first_name, user_data['first_name'])
             
-            response = self.client.post(url, user_data, format='json')
+    def success_response(self):
+        response = self.client.get(reverse('user_list', kwargs={'user_id':1}))
+        self.assertContains(response, 'user_list')   
+            
+            
+    def update_info(self):
+        url = reverse('user_info', args=[self.first_user.id])
+            
+        response = self.client.put(url, self.sample_data)
 
-            self.asserEqual(models.User.objects.count(), 2)
-            new_user = models.User.objects.get(pk=2)
-            self.asserEqual(new_user.first_name, user_data['first_name'])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
             
-        def success_response(self):
-            response = self.client.get(reverse('user_list', kwargs={'user_id':1}))
-            self.assertContains(response, 'user_list')   
-            
-            
-        def update_info(self):
-            url = reverse('user_info', args=[self.first_user.id])
-            
-            response = self.client.put(url, self.sample_data)
-
-            self.assertEqual(response.status_code, status.HTTP_200_OK)
-            
-        def delete_user(self):
-            url = reverse('user_info', args=[self.first_user.id])
-            response = self.client.delete(url)
-            self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    def delete_user(self):
+        url = reverse('user_info', args=[self.first_user.id])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
             
-          def fails_invalid(self):
-            response = self.client.get(reverse('user_list'))
-            self.assertEquals(response.status_code, 404)
-           
-        def tearDown(swlf):
-            pass
-            
+    def fails_invalid(self):
+        response = self.client.get(reverse('user_list'))
+        self.assertEquals(response.status_code, 404)
+              
+    def tearDown(self):
+        pass
+        
 
     
--- grubbr_models/models/urls.py
+++ grubbr_models/models/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import include, url
 from .services import *
 
 urlpatterns = [
-    url(r'^user/$', create_user),
-    url(r'^meal/$', create_meal),
-    url(r'^user/(?P<user_id>[0-9]+)$', rud_user_by_id),
-    url(r'^meal/(?P<meal_id>[0-9]+)$', rud_meal_by_id),
+    url(r'^user/$', create_user, name='user_create'),
+    url(r'^meal/$', create_meal, name='meal_create'),
+    url(r'^user/(?P<user_id>[0-9]+)$', rud_user_by_id, name='user_action'),
+    url(r'^meal/(?P<meal_id>[0-9]+)$', rud_meal_by_id, name='meal_action'),
 ]
