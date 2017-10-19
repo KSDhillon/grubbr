332a09f4acf3105900f7e0600400cf75527202bd
dankramp <danielbkramp@gmail.com>
2017-10-19 17:43:05 -0400

    Added forms, login functionality

--- grubbr_exp/exp/urls.py
+++ grubbr_exp/exp/urls.py
@@ -4,4 +4,5 @@ from .services import *
 urlpatterns = [
     url(r'^meals/$', get_home_page, name='homepage'),
     url(r'^meal/(?P<meal_id>[0-9]+)$', get_detail_page , name='detail'),
+    url(r'^login/$', login, name='exp_login'),
 ]
--- grubbr_web/web/views.py
+++ grubbr_web/web/views.py
@@ -3,6 +3,7 @@ import urllib.request
 import urllib.parse
 from django.template import loader
 import json
+from . import forms
 
 def home(request):
     req = urllib.request.Request('http://exp-api:8000/api/meals')
@@ -23,9 +24,32 @@ def meal(request, meal_id):
 def login(request):
 
     if request.method == 'GET':
-        return render(request, 'login.html')
+        return render(request, 'login.html', {'form': forms.LoginForm()})
     
-    return HttpResponse(template.render(request))
+    form = forms.LoginForm(request.POST)
+
+    if not form.is_valid():
+        return render(request, 'login.html') # send an error if not valid
+
+    email = form.cleaned_data['email']
+    password = form.cleaned_data['password']
+
+    data_enc = urllib.parse.urlencode([('email', email), ('password', password)]).encode('utf-8')
+    req = urllib.request.Request('http://exp-api:8000/api/login/', data_enc)
+
+    res = urllib.request.urlopen(req).read().decode('utf-8')
+
+    resp = json.loads(res)
+
+    if not resp or not resp['success']: # If login unsuccessful
+        return render(request, 'login.html', {'error': resp['result'] or "Error logging in"})
+    
+    auth = resp['result']
+
+    response = HttpResponseRedirect(reverse('home'))
+    response.set_cookie("auth", auth)
+    
+    return response
 
 def register(request):
 
