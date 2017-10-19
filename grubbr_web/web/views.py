282f0b6ed74ff92c10824543e3af2d2577963634
dankramp <danielbkramp@gmail.com>
2017-10-19 16:32:56 -0400

    Changed HTTPResponse to render()

--- grubbr_web/web/views.py
+++ grubbr_web/web/views.py
@@ -5,27 +5,28 @@ from django.template import loader
 import json
 
 def home(request):
-    template = loader.get_template('home.html')
     req = urllib.request.Request('http://exp-api:8000/api/meals')
     resp_json = urllib.request.urlopen(req).read().decode('utf-8')
     context = {
         'data': json.loads(resp_json)["result"]
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'home.html', context)
 
 def meal(request, meal_id):
-    template = loader.get_template('meal.html')
     req = urllib.request.Request('http://exp-api:8000/api/meal/' + str(meal_id))
     resp_json = urllib.request.urlopen(req).read().decode('utf-8')
     context = {
         'data': json.loads(resp_json)["result"]
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'meal.html', context)
 
 def login(request):
-    template = loader.get_template('login.html')
+
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    
     return HttpResponse(template.render(request))
 
 def register(request):
-    template = loader.get_template('register.html')
-    return HttpResponse(template.render(request))
+
+    return render(request, 'register.html', context)
