b53a5f3a1297c01e80750ef3ecc74598d736b69d
root <aadil.abbas96@gmail.com>
2017-10-20 01:30:53 +0000

    updated urls

--- grubbr_models/models/urls.py
+++ grubbr_models/models/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from .services import *
 
 urlpatterns = [
-    url(r'^register/$', create_user, name='user_create'),
+    url(r'^user/$', create_user, name='user_create'),
     url(r'^meal/$', create_meal, name='meal_create'),
     url(r'^login/$', login_user, name='login_user'),
     url(r'^user/(?P<user_id>[0-9]+)$', rud_user_by_id, name='user_action'),
